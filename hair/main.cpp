c91dff69d82c697c5bbd3e963787b90ec6fb458d
20k <the_berronator@hotmail.co.uk>
2016-02-06 05:13:18 +0000

    hair begins

--- hair/main.cpp
+++ hair/main.cpp
@@ -1,5 +1,9 @@
 #include "../openclrenderer/proj.hpp"
 
+#include <vec/vec.hpp>
+
+#include <boost/compute/core.hpp>
+
 ///todo eventually
 ///split into dynamic and static objects
 
@@ -10,14 +14,153 @@
 
 ///rift head movement is wrong
 
-//compute::event render_tris(engine& eng, cl_float4 position, cl_float4 rotation, compute::opengl_renderbuffer& g_screen_out);
-
-void callback (cl_event event, cl_int event_command_exec_status, void *user_data)
+void hair_load(objects_container* pobj)
 {
-    //printf("%s\n", user_data);
+    texture tex;
+    tex.type = 0;
+    tex.set_texture_location("res/red.png");
+    tex.push();
 
-    std::cout << (*(sf::Clock*)user_data).getElapsedTime().asMicroseconds()/1000.f << std::endl;
+    object obj;
+    obj.two_sided = 1;
+
+    obj.tid = tex.id;
+
+    obj.isloaded = true;
+
+    int segments = 10;
+    float length = 100;
+
+    float scale = length / segments;
+    float width = 5.f; ///ie offset from centre
+
+    std::vector<triangle> tri_list;
+
+    for(int i=0; i<segments; i++)
+    {
+        triangle tri;
+
+        for(int i=0; i<3; i++)
+            tri.vertices[i].set_normal({1, 1, 0});
+
+        for(int i=0; i<3; i++)
+            tri.vertices[i].set_vt({0.3f, 0.5f});
+
+        triangle tri2;
+
+        for(int i=0; i<3; i++)
+            tri2.vertices[i].set_normal({1, 1, 0});
+
+        for(int i=0; i<3; i++)
+            tri2.vertices[i].set_vt({0.3f, 0.5f});
+
+        quad q;
+        q.p1 = { i * scale, width, 0.f};
+        q.p2 = { i * scale, -width, 0.f};
+        q.p3 = { (i + 1) * scale, width, 0.f};
+        q.p4 = { (i + 1) * scale, -width, 0.f};
+
+        std::array<cl_float4, 6> pos = q.decompose();
+
+        tri.vertices[0].set_pos(pos[0]);
+        tri.vertices[1].set_pos(pos[1]);
+        tri.vertices[2].set_pos(pos[2]);
+
+        tri2.vertices[0].set_pos(pos[3]);
+        tri2.vertices[1].set_pos(pos[4]);
+        tri2.vertices[2].set_pos(pos[5]);
+
+        tri_list.push_back(tri);
+        tri_list.push_back(tri2);
+    }
+
+    obj.tri_list = tri_list;
+    obj.tri_num = tri_list.size();
+
+    obj.isloaded = true;
+
+    pobj->objs.push_back(obj);
+
+    pobj->isloaded = true;
 }
+
+template<typename T>
+struct double_buf
+{
+    int n = 0;
+    T bufs[2];
+
+    void flip()
+    {
+        n = (n + 1) % 2;
+    }
+
+    T cur()
+    {
+        return bufs[n];
+    }
+
+    T next()
+    {
+        return bufs[(n + 1) % 2];
+    }
+
+    T old()
+    {
+        return next();
+    }
+
+    T operator[](int i) const
+    {
+        return bufs[i];
+    }
+
+    T& operator[](int i)
+    {
+        return bufs[i];
+    }
+};
+
+///the stall may be because the cape functions write to tris
+///this may leave a hole
+///although i don't know why it would
+///investigate, with stringy bits!
+///also, stringy bits needs to be generic
+///able to deform dynamic objects
+///ie proper bone stuff
+///ie we take a model of length x, then deform along a bone based on the bone
+///we want thick dwarven tassles
+namespace compute = boost::compute;
+
+struct hair
+{
+    float len = 100;
+    int segments = 10;
+
+    double_buf<compute::buffer> seg_buf;
+
+    void init()
+    {
+        float scale = len / segments;
+
+        ///i literally cant see any reason why this wouldn't work
+        ///everywhere else on the face of the planet i'm using arrays of cl_float
+        std::vector< vec<3, cl_float> > to_init;
+
+        for(int i=0; i<segments; i++)
+        {
+            to_init.push_back({segments * scale, 0.f, 0.f});
+        }
+
+        for(int i=0; i<2; i++)
+            seg_buf[i] = compute::buffer(cl::context, segments * sizeof(to_init.front()), CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, to_init.data());
+    }
+
+    void tick()
+    {
+
+    }
+};
 
 ///gamma correct mipmap filtering
 ///7ish pre tile deferred
@@ -27,16 +170,17 @@ int main(int argc, char *argv[])
 
     object_context context;
 
-    auto sponza = context.make_new();
-    sponza->set_file("../openclrenderer/sp2/sp2.obj");
-    sponza->set_active(true);
-    sponza->cache = false;
+    objects_container* hair = context.make_new();
+    //hair->set_file("../openclrenderer/sp2/sp2.obj");
+    hair->set_load_func(hair_load);
+    hair->set_active(true);
+    hair->cache = false;
 
     engine window;
 
     window.load(1680,1050,1000, "turtles", "../openclrenderer/cl2.cl", true);
 
-    window.set_camera_pos((cl_float4){-800,150,-570});
+    window.set_camera_pos((cl_float4){-0,0,-0});
 
     //window.window.setPosition({-20, -20});
 
@@ -47,7 +191,8 @@ int main(int argc, char *argv[])
     ///we need a context.unload_inactive
     context.load_active();
 
-    sponza->set_specular(0.f);
+    //hair->scale(100.f);
+    hair->set_pos({0,0,100});
 
     texture_manager::allocate_textures();
 
@@ -62,7 +207,7 @@ int main(int argc, char *argv[])
 
     light l;
     l.set_col((cl_float4){1.0f, 1.0f, 1.0f, 0.0f});
-    l.set_shadow_casting(1);
+    l.set_shadow_casting(0);
     l.set_brightness(1);
     l.radius = 100000;
     l.set_pos((cl_float4){-200, 500, -100, 0});
@@ -73,7 +218,7 @@ int main(int argc, char *argv[])
     l.set_col((cl_float4){0.0f, 0.0f, 1.0f, 0});
 
     l.set_pos((cl_float4){-0, 200, -500, 0});
-    l.set_shadow_casting(1);
+    l.set_shadow_casting(0);
     l.radius = 100000;
 
     light::add_light(&l);
@@ -84,7 +229,7 @@ int main(int argc, char *argv[])
     window.set_light_data(light_data);
     window.construct_shadowmaps();
 
-    sf::Texture updated_tex;
+    /*sf::Texture updated_tex;
     updated_tex.loadFromFile("../openclrenderer/Res/test.png");
 
     for(auto& i : sponza->objs)
@@ -95,7 +240,7 @@ int main(int argc, char *argv[])
         {
             tex->update_gpu_texture(updated_tex, tex_gpu);
         }
-    }
+    }*/
 
 
     sf::Keyboard key;
