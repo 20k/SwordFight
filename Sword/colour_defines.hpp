0bd471732c132eb138e275cd1a63157520b53619
20k <the_berronator@hotmail.co.uk>
2016-02-23 16:05:51 +0000

    tophat, v0.1

---
+++ Sword/colour_defines.hpp
@@ -0,0 +1,13 @@
+#ifndef COLOUR_DEFINES_HPP_INCLUDED
+#define COLOUR_DEFINES_HPP_INCLUDED
+
+#include <vec/vec.hpp>
+
+namespace colours
+{
+    const static vec3f team_red = {248, 63, 95};
+    const static vec3f team_blue = {63, 95, 248};
+}
+
+
+#endif // COLOUR_DEFINES_HPP_INCLUDED
