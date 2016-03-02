5907b0d94d23b495a1814a7fd88fc78fda2a9144
20k <the_berronator@hotmail.co.uk>
2016-03-02 05:37:50 +0000

    more team cols

--- sword_server/teaminfo_shared.cpp
+++ sword_server/teaminfo_shared.cpp
@@ -7,11 +7,21 @@ vec3f team_info::get_team_col(int team)
 
     //return {0, 255, 0};
 
+    //return {255, 105, 180};
+
     if(team == 0)
         return rcol;
 
     if(team == 1)
         return bcol;
+
+    ///gold. temp basically
+    if(team == 2)
+        return {255, 215, 0};
+
+    ///pink
+    if(team == 3)
+        return {255, 105, 180};
 
     return {255, 255, 255};
 }
