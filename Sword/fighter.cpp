dc275855f5f192b625f2449e24e7e010e71bfd62
20k <the_berronator@hotmail.co.uk>
2016-06-10 03:57:59 +0100

    everything broken, except lights fixed?

--- Sword/fighter.cpp
+++ Sword/fighter.cpp
@@ -2930,7 +2930,22 @@ void fighter::update_lights()
 
     ///global_rot and a global pos here?
     ///maybe we should transform externally as well
-    vec3f bpos = (vec3f){0, parts[bodypart::BODY].global_pos.v[1] + 50.f, -150.f}.rot({0,0,0}, rot) + (vec3f){pos.v[0], 0.f, pos.v[2]};
+    ///this one incorrect for on walls due to pos
+    ///take head->body vector and use that
+    //vec3f bpos = (vec3f){0, parts[bodypart::BODY].global_pos.v[1] + 50.f, -150.f}.rot({0,0,0}, rot) + (vec3f){pos.v[0], 0.f, pos.v[2]};
+
+    vec3f body = parts[bodypart::BODY].global_pos;
+
+    vec3f head = parts[bodypart::HEAD].global_pos;
+
+    vec3f body_to_head = head - body;
+
+    vec3f body_to_right = parts[bodypart::RUPPERARM].global_pos - body;
+
+    vec3f in_front = cross(body_to_head.norm(), body_to_right.norm());
+
+    vec3f bpos = in_front.norm() * 150.f + body;
+
 
     my_lights[1]->set_pos({bpos.v[0], bpos.v[1], bpos.v[2]});
 
