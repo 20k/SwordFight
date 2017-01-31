459beffedcff8e623b9602bc605d069a5d625370
20k <the_berronator@hotmail.co.uk>
2017-01-31 14:54:07 +0000

    somewhat better mouse movement

--- sword_mapeditor/main.cpp
+++ sword_mapeditor/main.cpp
@@ -1,5 +1,8 @@
 #include "../openclrenderer/proj.hpp"
 #include "tinydir/tinydir.h"
+#include "../imgui/imgui.h"
+#include "../imgui/imgui-SFML.h"
+
 
 ///todo eventually
 ///split into dynamic and static objects
@@ -69,6 +72,51 @@ void load_level(objects_container* obj)
     return load_map(obj, 100, 100);
 }
 #endif
+
+///has the button been pressed once, and only once
+template<sf::Keyboard::Key k>
+bool once()
+{
+    static bool last;
+
+    sf::Keyboard key;
+
+    if(key.isKeyPressed(k) && !last)
+    {
+        last = true;
+
+        return true;
+    }
+
+    if(!key.isKeyPressed(k))
+    {
+        last = false;
+    }
+
+    return false;
+}
+
+template<sf::Mouse::Button b>
+bool once()
+{
+    static bool last;
+
+    sf::Mouse m;
+
+    if(m.isButtonPressed(b) && !last)
+    {
+        last = true;
+
+        return true;
+    }
+
+    if(!m.isButtonPressed(b))
+    {
+        last = false;
+    }
+
+    return false;
+}
 
 void load_floor(objects_container* obj)
 {
@@ -143,6 +191,8 @@ struct asset_manager
 {
     std::vector<asset> assets;
 
+    int last_hovered = -1;
+
     void populate(const std::string& asset_dir)
     {
         tinydir_dir dir;
@@ -212,6 +262,220 @@ struct asset_manager
             a.loaded_asset->set_dynamic_scale(100.f);
         }
     }
+
+    void patch_textures()
+    {
+        for(asset& a : assets)
+        {
+            a.loaded_asset->patch_non_2pow_texture_maps();
+        }
+    }
+
+    void set_last_hovered(objects_container* c)
+    {
+        for(int i=0; i<assets.size(); i++)
+        {
+            if(assets[i].loaded_asset == c)
+            {
+                last_hovered = i;
+                return;
+            }
+        }
+
+        last_hovered = -1;
+    }
+
+    void do_ui()
+    {
+        ImGui::Begin("Currently Hovering");
+
+        std::string hovered_name;
+
+        if(last_hovered != -1)
+            hovered_name = assets[last_hovered].loaded_asset->file;
+
+        ImGui::Button(hovered_name.c_str());
+
+        ImGui::End();
+
+        std::map<std::string, std::vector<int>> directory_to_asset_name;
+
+        for(int i=0; i<assets.size(); i++)
+        {
+            directory_to_asset_name[assets[i].asset_toplevel_directory].push_back(i);
+        }
+
+
+        ImGui::Begin("Asset list");
+
+        /*for(int i=0; i<assets.size(); i++)
+        {
+            asset& a = assets[i];
+
+            //ImGui::CollapsingHeader
+
+            //ImGui::Button(a.loaded_asset->file.c_str(), ImVec2(0, 14));
+        }*/
+
+        for(auto& i : directory_to_asset_name)
+        {
+            std::string dirname = i.first;
+            const std::vector<int>& asset_list = i.second;
+
+            if(ImGui::CollapsingHeader(dirname.c_str()))
+            {
+                for(auto& j : asset_list)
+                {
+                    asset& a = assets[j];
+
+                    ImGui::Button(a.asset_name.c_str());
+                }
+            }
+        }
+
+        ImGui::End();
+    }
+
+    void do_interactivity(engine& window)
+    {
+        sf::Keyboard key;
+        sf::Mouse mouse;
+
+        float dx = window.get_mouse_delta_x();
+        float dy = window.get_mouse_delta_y();
+
+        /*if(once<sf::Mouse::Left>() && last_hovered != -1)
+        {
+            asset& a = assets[last_hovered];
+
+
+        }*/
+
+        if(last_hovered == -1)
+            return;
+
+        float fov_const = calculate_fov_constant_from_hfov(window.horizontal_fov_degrees, window.width);
+
+        asset& a = assets[last_hovered];
+
+        cl_float4 pos = a.loaded_asset->pos;
+
+        vec3f object_pos = {pos.x, pos.y, pos.z};
+
+        vec3f c_pos = {window.c_pos.x, window.c_pos.y, window.c_pos.z};
+
+        vec3f camera_to_object = object_pos - c_pos;
+
+        float object_depth = camera_to_object.length();
+
+        vec3f object_rot = object_pos.rot({window.c_pos.x, window.c_pos.y, window.c_pos.z}, {window.c_rot.x, window.c_rot.y, window.c_rot.z});
+        vec3f object_proj = {object_rot.x() * fov_const / object_rot.z(), object_rot.y() * fov_const / object_rot.z(), object_rot.z()};
+
+        object_proj.x() = object_proj.x() + window.width/2.f;
+        object_proj.y() = object_proj.y() + window.height/2.f;
+
+        vec3f new_pos = {object_proj.x() + dx, object_proj.y() + -dy, object_proj.z()};
+
+        float yplane = object_pos.y();
+
+        vec3f back_projected_new_pos = {new_pos.x() - window.width/2.f, new_pos.y() - window.height/2, new_pos.z()};
+
+        back_projected_new_pos = {back_projected_new_pos.x() * back_projected_new_pos.z() / fov_const, back_projected_new_pos.y() * back_projected_new_pos.z() / fov_const, back_projected_new_pos.z()};
+
+        vec3f camera_ray = back_projected_new_pos.back_rot({0,0,0}, {window.c_rot.x, window.c_rot.y, window.c_rot.z});
+
+        vec3f intersect_point = ray_plane_intersect(camera_ray, c_pos, (vec3f){0, 1, 0}, (vec3f){0, yplane, 0});
+
+        ///really we want distance to projection plane (?) but... close enough, this is only for controls
+
+        vec3f current_relative_mouse_projected_pos = {dx * object_depth / fov_const, dy * object_depth / fov_const, 0.f};
+
+        float move_len = current_relative_mouse_projected_pos.xy().length();
+
+        vec3f move_axis = {0, 0, 0};
+
+        float len = sqrt(dx*dx + dy*dy);
+
+        /*if(mouse.isButtonPressed(sf::Mouse::Left))
+        {
+            move_axis.x() = 1;
+        }
+
+        if(mouse.isButtonPressed(sf::Mouse::XButton1))
+        {
+            move_axis.y() = 1;
+        }
+
+        if(mouse.isButtonPressed(sf::Mouse::Right))
+        {
+            move_axis.z() = 1;
+        }
+
+        if(move_axis.x() == 1)
+        {
+            //if(dx < 0)
+            //    move_axis.x() = -move_axis.x();
+
+            move_axis.x() = move_axis.x() * current_relative_mouse_projected_pos.x();
+        }
+
+        if(move_axis.y() == 1)
+        {
+            //if(dy > 0)
+            //    move_axis.y() = -move_axis.y();
+
+            move_axis.y() = move_axis.y() * -current_relative_mouse_projected_pos.y();
+        }
+
+        if(move_axis.z() == 1)
+        {
+            move_axis.z() = move_axis.z() * current_relative_mouse_projected_pos.x();
+        }*/
+
+        ///new control scheme, hold left click for xz axis, right click for y axis
+
+        /*if(mouse.isButtonPressed(sf::Mouse::Left))
+        {
+            move_axis.x() = 1;
+            move_axis.z() = 1;
+        }*/
+
+        if(mouse.isButtonPressed(sf::Mouse::Right))
+        {
+            move_axis.y() = 1;
+        }
+
+        if(move_axis.x() == 1)
+        {
+            //if(dx < 0)
+            //    move_axis.x() = -move_axis.x();
+
+            move_axis.x() = move_axis.x() * current_relative_mouse_projected_pos.x();
+        }
+
+        if(move_axis.y() == 1)
+        {
+            move_axis.y() = move_axis.y() * -current_relative_mouse_projected_pos.y();
+        }
+
+        if(move_axis.z() == 1)
+        {
+            move_axis.z() = move_axis.z() * -current_relative_mouse_projected_pos.y();
+        }
+
+        //move_axis = move_axis.norm() * move_len;
+
+        vec3f spos = object_pos + move_axis;
+
+        a.loaded_asset->set_pos({spos.x(), spos.y(), spos.z()});
+
+        if(mouse.isButtonPressed(sf::Mouse::Left))
+        {
+            spos = (vec3f){intersect_point.x(), intersect_point.y(), intersect_point.z()};
+
+            a.loaded_asset->set_pos({spos.x(), spos.y(), spos.z()});
+        }
+    }
 };
 
 ///gamma correct mipmap filtering
@@ -241,9 +505,14 @@ int main(int argc, char *argv[])
 
     engine window;
 
+    window.append_opencl_extra_command_line("-DCAN_OUTLINE");
     window.load(1680,1050,1000, "turtles", "../openclrenderer/cl2.cl", true);
 
     window.set_camera_pos({0, 285.298, -900});
+
+    ImGui::SFML::Init(window.window);
+
+    window.raw_input_set_active(true);
 
     asset_manager asset_manage;
     asset_manage.populate("Assets");
@@ -254,6 +523,7 @@ int main(int argc, char *argv[])
     context.load_active();
 
     asset_manage.scale();
+    asset_manage.patch_textures();
 
     sponza->scale(100.f);
 
@@ -306,6 +576,11 @@ int main(int argc, char *argv[])
     window.window.setIcon(crab.getSize().x, crab.getSize().y, crab.getPixelsPtr());
 
     sf::Keyboard key;
+    sf::Mouse mouse;
+
+    sf::Clock deltaClock;
+
+    objects_container* last_hovered = nullptr;
 
     ///use event callbacks for rendering to make blitting to the screen and refresh
     ///asynchronous to actual bits n bobs
@@ -314,10 +589,80 @@ int main(int argc, char *argv[])
     {
         sf::Clock c;
 
+        window.reset_scrollwheel_delta();
+        window.raw_input_process_events();
+
         while(window.window.pollEvent(Event))
         {
             if(Event.type == sf::Event::Closed)
                 window.window.close();
+
+            ImGui::SFML::ProcessEvent(Event);
+        }
+
+        ImGui::SFML::Update(deltaClock.restart());
+
+        int last_hovered_asset = -1;
+
+        if(!mouse.isButtonPressed(sf::Mouse::Left) && !mouse.isButtonPressed(sf::Mouse::Right) && !mouse.isButtonPressed(sf::Mouse::XButton1))
+        {
+            cl_int frag_id = -1;
+            cl_uint depth = -1;
+
+            int x = window.get_mouse_x();
+            int y = window.height - window.get_mouse_y();
+
+            size_t origin[3] = {window.get_mouse_x(), window.height - window.get_mouse_y(), 0};
+            size_t region[3] = {1, 1, 1};
+
+            ///if we wanted to do this better, we could set up a clseteventcallback on the completion of the read of frag id, and use that to enqueue the next read
+            ///that way we'd only have to stall explicitly once at the end. There might be an implicit stall in the middle though, but its possible that the other read will be overlapped which might take out some of the pain
+            ///but really thats not going to be enough time to remove a stall. This needs testing. We could overlap it with shadow generation
+            if(origin[0] >= 0 && origin[0] < window.width && origin[1] >= 0 && origin[1] < window.height)
+            {
+                clEnqueueReadImage(cl::cqueue.get(), context.fetch()->g_id_screen_tex.get(), CL_FALSE, origin, region, 0, 0, &frag_id, 0, nullptr, nullptr);
+                clEnqueueReadBuffer(cl::cqueue.get(), context.fetch()->depth_buffer[1].get(), CL_FALSE, sizeof(cl_uint) * (y * window.width + x), sizeof(cl_uint), &depth, 0, nullptr, nullptr);
+            }
+
+            cl::cqueue.finish();
+
+            cl_uint o_id = -1;
+
+            if(frag_id >= 0)
+                clEnqueueReadBuffer(cl::cqueue.get(), window.g_tid_buf.get(), CL_FALSE, sizeof(cl_uint) * (frag_id * 6 + 5), sizeof(cl_uint), &o_id, 0, nullptr, nullptr);
+
+            cl::cqueue.finish();
+
+            int object_id = context.translate_gpu_o_id_to_container_offset(o_id);
+
+            for(auto& i : context.containers)
+            {
+                i->set_outlined(false);
+            }
+
+            if(object_id != -1 && depth != -1)
+            {
+                last_hovered = context.containers[object_id];
+            }
+            else
+            {
+                last_hovered = nullptr;
+            }
+
+            if(last_hovered)
+            {
+                last_hovered->set_outlined(true);
+            }
+
+            asset_manage.set_last_hovered(last_hovered);
+
+            /*float actual_depth = ((float)depth / UINT_MAX) * 350000.f;
+
+            float fov = 500.f; ///will need to use real value
+
+            vec3f local_position = {((x - window.width/2.0f)*actual_depth/fov), ((y - window.height/2.0f)*actual_depth/fov), actual_depth};
+            vec3f global_position = local_position.back_rot(0.f, {window.c_rot.x, window.c_rot.y, window.c_rot.z});
+            global_position += (vec3f){window.c_pos.x, window.c_pos.y, window.c_pos.z};*/
         }
 
         compute::event event;
@@ -339,6 +684,13 @@ int main(int argc, char *argv[])
 
         window.render_block();
 
+        window.window.resetGLStates();
+
+        asset_manage.do_ui();
+        asset_manage.do_interactivity(window);
+
+        ImGui::Render();
+
         window.flip();
 
         context.flush_locations();
@@ -351,6 +703,7 @@ int main(int argc, char *argv[])
             std::cout << c.getElapsedTime().asMicroseconds() << std::endl;
     }
 
+    ImGui::SFML::Shutdown();
     ///if we're doing async rendering on the main thread, then this is necessary
     cl::cqueue.finish();
     glFinish();
