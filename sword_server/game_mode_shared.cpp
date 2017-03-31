eedb904f9ad48c43e8f01154717c1609184cd09d
20k <the_berronator@hotmail.co.uk>
2017-03-31 01:57:26 +0100

    moving towards per-player stats

--- sword_server/game_mode_shared.cpp
+++ sword_server/game_mode_shared.cpp
@@ -23,3 +23,25 @@ std::string game_mode_handler_shared::get_display_string()
 {
     return current_session_state.get_current_game_state_string(current_session_boundaries);
 }
+
+
+void game_mode_handler_shared::register_kill(int32_t id_who_killed, int32_t id_who_died, int32_t team_who_killed, int32_t team_who_died)
+{
+    if(id_who_killed < 0)
+        return;
+
+    if(id_who_died < 0)
+        return;
+
+    if(team_who_died < 0 || team_who_died >= TEAM_NUMS)
+        return;
+
+    if(team_who_killed < 0 || team_who_killed >= TEAM_NUMS)
+        return;
+
+    player_info[id_who_killed].kills++;
+    player_info[id_who_died].deaths++;
+
+    current_session_state.team_kills[team_who_killed]++;
+    current_session_state.team_killed[team_who_died]++;
+}
--- sword_server/game_mode_shared.hpp
+++ sword_server/game_mode_shared.hpp
@@ -3,6 +3,7 @@
 
 #include "game_server/game_modes.hpp"
 #include <SFML/System.hpp>
+#include <map>
 
 struct server_game_state;
 
@@ -12,12 +13,23 @@ struct server_game_state;
 ///Ok. First decision. This class IS going to know if its client or server
 ///we're going to keep all gamemode logic in one place EVEN THOUGH aspects of this won't be run for each client
 ///This will make it a lot more straightforward to manage gamemodes as EVERYTHING (pretty much) will be in ONE place-ish
+///ok. Keep player stats in here relayed from server to client
+
+struct player_info_shared
+{
+    int kills = 0;
+    int assists = 0;
+    int deaths = 0;
+};
+
 struct game_mode_handler_shared
 {
     game_mode_t current_game_mode = game_mode::FIRST_TO_X;
 
     session_state current_session_state;
     session_boundaries current_session_boundaries;
+
+    std::map<int32_t, player_info_shared> player_info;
 
     std::string get_display_string();
     std::string get_game_over_string();
@@ -26,6 +38,8 @@ struct game_mode_handler_shared
     void tick(bool is_client);
 
     bool game_over();
+
+    void register_kill(int32_t id_who_killed, int32_t id_who_died, int32_t team_who_killed, int32_t team_who_died);
 
     ///useless on client
     bool in_game_over_state = false;
